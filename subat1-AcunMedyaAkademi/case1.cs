63b783f83c1e2c340a3e1b59536628fcaf11d669
tunadeveloper <tunabusiness25@gmail.com>
2025-03-15 19:58:32 +0300

    Add project files.

---
+++ subat1-AcunMedyaAkademi/case1.cs
@@ -0,0 +1,13 @@
+﻿int[] numbers = new int[5];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"Lütfen {i + 1}. sayıyı girin: ");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Girilen Sayılar:");
+foreach (int num in numbers)
+{
+    Console.Write(num + " ");
+}
---
+++ subat1-AcunMedyaAkademi/case2.cs
@@ -0,0 +1,12 @@
+﻿
+        int[] numbers = { 15, 42, 7, 89, 23 };
+        int max = numbers[0];
+
+        foreach (int num in numbers)
+        {
+            if (num > max)
+                max = num;
+        }
+
+        Console.WriteLine("En büyük sayı: " + max);
+    }
